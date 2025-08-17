dd12b58adb6519954bd3bf9501340e796f8dfa55
ArthurPSampaio <arthurasampaio@gmail.com>
2025-08-17 18:03:53 -0300

    Initial commit: Setup NestJS project with PostgreSQL

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    // Carrega as variáveis do arquivo .env
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna as variáveis disponíveis globalmente
+    }),
+
+    // Configuração do TypeORM usando as variáveis de ambiente
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
